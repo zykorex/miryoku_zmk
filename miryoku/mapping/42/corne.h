8ef79ed5e4aa8573c5352ebe34587cd400dde190
zykorex <omair.malik+github@gmail.com>
2024-08-17 00:19:46 -0700

    Update corne.h to show extra keys on start/end columns for al layers

--- miryoku/mapping/42/corne.h
+++ miryoku/mapping/42/corne.h
@@ -4,6 +4,10 @@
 #if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
 
 #define XXX &none
+#define XLT  &kp TAB
+#define XLM  &caps_word
+#define XLB  &kp LSHIFT
+#define XRM  &key_repeat
 
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -12,8 +16,8 @@
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+XLM  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XRM \
+XLB  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
